3d34a7a07cced3e282adbecc2a5cf677f7305be7
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-11-04 16:28:47 +0900

    Update 5.py

--- L3/5.py
+++ L3/5.py
@@ -1,26 +1,19 @@
 def main():
-    p = False
+    while True:
     a = int(input())
+    if a != 0:
+        if a % 3 == 0 and a % 7 == 0:
+            print('Караул!')
+            break
     
-    if a % 3 == 0 and a % 7 == 0:
-        print('Караул!')
+        elif a % 3 == 0 or a % 7 == 0:
+            print(type(a))
     
-    elif a % 3 == 0 or a % 7 == 0:
-        print(type(a))
-    
+        else:
+            print(a)
+
     else:
-        while True:
-            a = int(input())
-    
-            if a % 3 == 0 and a % 7 == 0:
-                print('Караул!')
-                break
-    
-            elif a % 3 == 0 or a % 7 == 0:
-                print(type(a))
-    
-            else:
-                print(a)
+        break
 
 if __name__ == '__main__':
     main()
