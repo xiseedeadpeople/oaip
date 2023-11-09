9b25cd6c126e342485be2942596c7d0b85dcd6ce
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-11-09 17:16:07 +0900

    Update and rename 4(КОД НЕДОПИСАН!).py to 4.py

--- L4/4.py
+++ L4/4.py
@@ -1,26 +1,29 @@
 def main():
     ch = input()
-    repeat_counter_string = 0
-    repeat_counter_vr = 1
-    vrem_max = []
-    
+    repcounter = 1
+    streakreps = []
+    mxinstrngs = []
+
     for k in range(int(input())):
         a = list(input())
-    
+
         inds = [i for i, j in enumerate(a) if j == ch]
         inds.append(-6)
-        print(inds)
-    
+
         for el in range(len(inds) - 1): # по списку индексов
-    
+
             if inds[el + 1] - inds[el] == 1:
-                repeat_counter_vr += 1
-    
+                repcounter += 1
+
             else: # если не повт
-                vrem_max.append(repeat_counter_vr)
-                repeat_counter_vr = 0
-    
-        print(vrem_max)
+                streakreps.append(repcounter)
+                repcounter = 1
+
+
+        mxinstrngs.append(max(streakreps))
+        streakreps = []
+
+    print(max(mxinstrngs))
 
 if __name__ == '__main__':
     main()
