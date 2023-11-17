93d531dad4727eceb7d17c30d002e8efb14a468a
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-11-17 22:17:14 +0900

    Create bubble.py

---
+++ L6/bubble.py
@@ -0,0 +1,13 @@
+def main():
+    l1 = [12, 5, 7, 2, 8]
+
+    for i in range(len(l1) - 1):
+        for j in range(len(l1) - i - 1):
+            if l1[j] > l1[j + 1]:
+                l1[j], l1[j + 1] = l1[j + 1], l[j]
+
+
+    print(l1)
+
+if __name__ == '__main__':
+    main()
