3886eaefe65ae9d732a50f030630e5400ddd282e
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-11-17 22:18:05 +0900

    Update bubble.py

--- L6/bubble.py
+++ L6/bubble.py
@@ -4,7 +4,7 @@ def main():
     for i in range(len(l1) - 1):
         for j in range(len(l1) - i - 1):
             if l1[j] > l1[j + 1]:
-                l1[j], l1[j + 1] = l1[j + 1], l[j]
+                l1[j], l1[j + 1] = l1[j + 1], l1[j]
 
 
     print(l1)
