b9eade2b631a5bf7cffae6963d9731e71be44720
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-12-02 22:48:51 +0900

    Update 2.py

--- L8/2.py
+++ L8/2.py
@@ -1,17 +1,18 @@
 def main():
-    l1 = '1000,1000000000000,2000;2000000000000000000000000,2132131212312323,33'.split(';')
+    l1 = input().split(';')
     l2 = [i.split(',') for i in l1]
     print(l2)
-    
+
     for rows in range(len(l2)):
         for el in range(len(l2[rows])):
-    
+
             if el + 1 == len(l2[rows]):
                 print()
-    
+
             else:
                 if int(l2[rows][el]) >= 1000000000:
                     print(l2[rows][el], end=' ')
-                    
+
+
 if __name__ == "__main__":
     main()
