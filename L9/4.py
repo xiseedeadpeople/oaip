89fac060b87325ec6534efe89b748fe35a54d698
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-12-08 03:57:06 +0900

    Update 4.py

--- L9/4.py
+++ L9/4.py
@@ -1,1 +1,11 @@
+def main():
+    try:
+        a = int(input())
+        b = int(input())
+        print(2 / 0)
+    
+    except (ZeroDivisionError, ValueError) as e:
+        print(e)
 
+if __name__ == "__main__":
+    main()
