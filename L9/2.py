ba97eaf13decaaa4ef021da705a8f55646d0ccdf
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2023-12-08 03:51:15 +0900

    Create 2.py

---
+++ L9/2.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        try:
+            a = int(input())
+            b = int(input())
+            break
+    
+        except ValueError:
+            print('ValueErr!')
+ if __name__ == "__main__":
+  main()
