911a839fd53436c56459dd97c81cc628f41b4820
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-03-01 15:54:07 +0900

    Update 3.py

--- ОП4/L5/3.py
+++ ОП4/L5/3.py
@@ -1,1 +1,14 @@
+def sequence(brackets):
+    chars = '({[<>]})'
 
+    for i in brackets:
+        if i not in chars:
+            brackets = brackets.replace(i, '')
+
+    while '()' in brackets or '[]' in brackets or '{}' in brackets or '<>' in brackets:
+        brackets = brackets.replace('()', '').replace('[]', '').replace('{}', '').replace('<>', '')
+
+    return brackets == ''
+
+if __name__ == "__main__":
+    print(sequence('12 / (9) + 2 ()'))
