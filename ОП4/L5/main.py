90666fa4fe9f6e8a25ed193af70390d53b44ebaf
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-03-18 12:36:42 +0900

    Create main.py

---
+++ ОП4/L5/main.py
@@ -0,0 +1,13 @@
+from brackets import brackets
+from gears import gears
+from groundhog_day import groundhog_day
+
+
+def main():
+    brackets()
+    gears()
+    groundhog_day()
+
+
+if __name__ == '__main__':
+    main()
