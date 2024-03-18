b3d8c20313f7dfa31b453e29e4e449c50cd09e87
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-03-18 13:27:06 +0900

    Create main.py

---
+++ ОП4/L8/main.py
@@ -0,0 +1,11 @@
+from stars import stars
+from nearby import nearby
+
+
+def main():
+    print(stars('takeshi kovacs concerto in f minor'))
+    print(nearby(['010101011', '000100000101']))
+
+
+if __name__ == '__main__':
+    main()
