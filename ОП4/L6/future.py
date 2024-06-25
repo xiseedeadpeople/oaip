292aedd0597e03321a30d693645eac0008a01f3f
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-06-25 22:36:41 +0900

    Update future.py

--- ОП4/L6/future.py
+++ ОП4/L6/future.py
@@ -1,16 +1,9 @@
 def future(*mass, **const):
-
-    VIN = 3
-    CountConst = 1
-
-    for key, value in const.items():
-        CountConst *= float(value)
-    sum_of_masses = sum(mass) * CountConst
-
-    if sum_of_masses > VIN:
-        return 'ACCELERATION'
-    
-    elif sum_of_masses < VIN:
-        return 'DECELERATION'
-
-    return 'UNCHANGED'
+    global VIN
+    sum_mass = sum(mass)
+    if VIN < sum_mass:
+        return "ACCELERATION"
+    elif VIN > sum_mass:
+        return "DECELERATION"
+    else:
+        return "UNCHANGED"
