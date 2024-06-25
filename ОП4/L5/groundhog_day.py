f8134b12ee69b603fd6307477c6dca7d7dd1be62
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-06-25 22:23:16 +0900

    Update groundhog_day.py

--- ОП4/L5/groundhog_day.py
+++ ОП4/L5/groundhog_day.py
@@ -1,8 +1,15 @@
-def groundhog_day(a):
-    for i, (prev, curr) in enumerate(zip(a, a[1:]), 1):
-        res = [i for i, (x, y) in enumerate(zip(prev, curr)) if x != y]
+def groundhog_day(data):
+    if len(data) < 2:
+        return (0, 0)
 
-        if len(res) > 2:
-            return (i,) + tuple(res)
-
+    for i in range(1, len(data)):
+        differences = 0
+        diff_indices = []
+        for j in range(len(data[i])):
+            if data[i][j] != data[i-1][j]:
+                differences += 1
+                diff_indices.append(j)
+                if differences > 2:
+                    return (i, *diff_indices)
+    
     return (0, 0)
