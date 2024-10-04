cb54be5f6ee744ef2318ce424ae36e8a610ceed3
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-10-04 12:23:10 +0900

    Update bridge.py

--- scr/bridge.py
+++ scr/bridge.py
@@ -1,4 +1,3 @@
-# Интерфейс устройства (Implementor)
 class Device:
     def power_on(self):
         pass
@@ -10,44 +9,42 @@ class Device:
         pass
 
 
-# Конкретные устройства (Concrete Implementors)
 class Television(Device):
     def __init__(self):
-        self._is_on = False
+        self.status = False
         self._volume = 50
 
     def power_on(self):
-        self._is_on = True
-        print("Телевизор включен.")
+        self.status = True
+        print('телевизор включен')
 
     def power_off(self):
-        self._is_on = False
-        print("Телевизор выключен.")
+        self.status = False
+        print('телевизор выключен')
 
     def set_volume(self, volume):
         self._volume = volume
-        print(f"Громкость телевизора установлена на {self._volume}.")
+        print(f'громкость телевизора установлена на {self._volume}')
 
 
 class Radio(Device):
     def __init__(self):
-        self._is_on = False
+        self.status = False
         self._volume = 30
 
     def power_on(self):
-        self._is_on = True
-        print("Радио включено.")
+        self.status = True
+        print('радио включено')
 
     def power_off(self):
-        self._is_on = False
-        print("Радио выключено.")
+        self.status = False
+        print('радио выключено')
 
     def set_volume(self, volume):
         self._volume = volume
-        print(f"Громкость радио установлена на {self._volume}.")
+        print(f'громкость радио установлена на {self._volume}')
 
 
-# Абстракция RemoteControl (Abstraction)
 class RemoteControl:
     def __init__(self, device: Device):
         self.device = device
@@ -62,25 +59,22 @@ class RemoteControl:
         self.device.set_volume(volume)
 
 
-# Расширенная абстракция для разных типов пультов (Refined Abstraction)
 class IRRemoteControl(RemoteControl):
     def __init__(self, device: Device):
         super().__init__(device)
-        print("Используется инфракрасный пульт.")
+        print('используется обычный пульт')
 
 
 class BluetoothRemoteControl(RemoteControl):
     def __init__(self, device: Device):
         super().__init__(device)
-        print("Используется Bluetooth пульт.")
+        print('используется bluetooth пульт')
 
 
-
-# Создаем устройства
 tv = Television()
 radio = Radio()
 
-# Инфракрасный пульт для телевизора
+# tv
 ir_remote_for_tv = IRRemoteControl(tv)
 ir_remote_for_tv.power_on()
 ir_remote_for_tv.set_volume(75)
@@ -88,7 +82,7 @@ ir_remote_for_tv.power_off()
 
 print()
 
-# Bluetooth пульт для радио
+# radio
 bt_remote_for_radio = BluetoothRemoteControl(radio)
 bt_remote_for_radio.power_on()
 bt_remote_for_radio.set_volume(40)
