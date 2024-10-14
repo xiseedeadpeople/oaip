05fef229075d8b6d0a0b48a31e34911894146210
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-10-14 20:22:36 +0900

    Update currency_converter.py

--- scr/currency_converter.py
+++ scr/currency_converter.py
@@ -10,22 +10,33 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(600, 200)
         self.setMaximumSize(1200, 400)
 
-        combobox1 = QComboBox()
-        combobox1.addItems(['USD', 'RUB', 'EUR', 'NZD'])
-        combobox1.setFixedSize(200, 50)
+        self.cbox1 = QComboBox()
+        self.cbox1.addItems(['USD', 'RUB', 'EUR', 'NZD'])
+        self.cbox1.setFixedSize(200, 50)
 
-        combobox2 = QComboBox()
-        combobox2.addItems(['RUB', 'USD', 'EUR', 'NZD'])
-        combobox2.setFixedSize(200, 50)
+        self.cbox2 = QComboBox()
+        self.cbox2.addItems(['RUB', 'USD', 'EUR', 'NZD'])
+        self.cbox2.setFixedSize(200, 50)
+
+        self.box_vals = self.cbox1.currentText(), self.cbox2.currentText()
+        self.cbox1.currentIndexChanged.connect(lambda: self.change_similiars(self.cbox1.currentText()))
 
         layout = QVBoxLayout()
-        layout.addWidget(combobox1)
-        layout.addWidget(combobox2)
+        layout.addWidget(self.cbox1)
+        layout.addWidget(self.cbox2)
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
+
+
+    def change_similiars(self, value):
+
+        if self.cbox1.currentText() == self.cbox2.currentText():
+            self.cbox1.setCurrentText(self.box_vals[0])
+            self.cbox2.setCurrentText(self.box_vals[1])
+
 
 
 
