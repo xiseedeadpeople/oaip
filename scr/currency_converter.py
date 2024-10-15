8e8e9f3d4bf4caba34982779ff2605aa3de59872
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-10-15 10:57:26 +0900

    Update currency_converter.py

--- scr/currency_converter.py
+++ scr/currency_converter.py
@@ -1,5 +1,31 @@
-from PyQt6.QtWidgets import QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton
+from bs4 import BeautifulSoup  # pip install requests lxml bs4
+import requests
 import sys
+
+
+url = 'https://www.cbr.ru/currency_base/daily/'
+
+responce = requests.get(url).text
+soup = BeautifulSoup(responce, 'lxml')
+nouns = soup.findAll('tr')
+
+allc = [[i.text] for i in nouns][1::]
+valutes, amount, currency = ['RUB'], [], []
+
+# too gross cycle
+for i in allc:
+    res = []
+
+    for x in i:
+        res.append(x.split())
+
+    for el in res:
+        valutes.append(res[0][1])
+        amount.append(res[0][2])
+        currency.append(res[0][-1])
+
+valutes.sort()
 
 
 class MainWindow(QMainWindow):
@@ -10,34 +36,40 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(600, 200)
         self.setMaximumSize(1200, 400)
 
+        # # # # #
+
         self.cbox1 = QComboBox()
-        self.cbox1.addItems(['USD', 'RUB', 'EUR', 'NZD'])
+        self.cbox1.addItems(valutes)
+        self.cbox1.setCurrentText('RUB')
         self.cbox1.setFixedSize(200, 50)
 
         self.cbox2 = QComboBox()
-        self.cbox2.addItems(['RUB', 'USD', 'EUR', 'NZD'])
+        self.cbox2.addItems(valutes)
+        self.cbox2.setCurrentText('USD')
         self.cbox2.setFixedSize(200, 50)
 
+        # # # # #
+
+        self.button = QPushButton('1')
+        self.button.setFixedSize(20, 20)
+
+        # widget.setSuffix("c") 'asd' -> 'asd$'
+
         self.box_vals = self.cbox1.currentText(), self.cbox2.currentText()
-        self.cbox1.currentIndexChanged.connect(lambda: self.change_similiars(self.cbox1.currentText()))
+
+        # # # # #
 
         layout = QVBoxLayout()
         layout.addWidget(self.cbox1)
         layout.addWidget(self.cbox2)
+        layout.addWidget(self.button)
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
 
-
-    def change_similiars(self, value):
-
-        if self.cbox1.currentText() == self.cbox2.currentText():
-            self.cbox1.setCurrentText(self.box_vals[0])
-            self.cbox2.setCurrentText(self.box_vals[1])
-
-
+        # # # # #
 
 
 app = QApplication(sys.argv)
