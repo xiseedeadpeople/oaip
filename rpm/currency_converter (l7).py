115f54f782afd1226762d772e392d8194c5e9564
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2024-10-22 10:59:16 +0900

    Update currency_converter (l7).py

--- rpm/currency_converter (l7).py
+++ rpm/currency_converter (l7).py
@@ -38,61 +38,62 @@ for i in allc:
 
 valutes.sort()
 
-print(d)
+# print(d)
 
-# class MainWindow(QMainWindow):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.setFixedSize(400, 130)
-#
-#         # # # # #
-#
-#         self.cbox1 = QComboBox()
-#         self.cbox1.addItems(valutes) # d.keys()
-#         self.cbox1.setCurrentText('RUB')
-#         self.cbox1.setFixedSize(190, 20)
-#
-#         self.cbox2 = QComboBox()
-#         self.cbox2.addItems(valutes)  # d.keys()
-#         self.cbox2.setCurrentText('USD')
-#         self.cbox2.setFixedSize(190, 20)
-#
-#         self.button = QPushButton('↑↓')
-#
-#         self.input1 = QLineEdit()
-#         self.input1.setFixedSize(190, 20)
-#         # self.input1.textChanged.connect()
-#
-#         self.input2 = QLineEdit()
-#         self.input2.setFixedSize(190, 20)
-#
-#         # widget.setSuffix("c") 'asd' -> 'asd$'
-#
-#         self.box_vals = self.cbox1.currentText(), self.cbox2.currentText()
-#
-#         # # # # #
-#
-#         layout = QGridLayout()
-#         layout.addWidget(self.cbox1, 0, 0)
-#         # layout.addWidget(self.button, 1, 0)
-#         layout.addWidget(self.cbox2, 2, 0)
-#
-#         layout.addWidget(self.input1, 0, 2)
-#         layout.addWidget(self.input2, 2, 2)
-#
-#         #
-#
-#         container = QWidget()
-#         container.setLayout(layout)
-#         self.setCentralWidget(container)
-#
-#         # # # # #
-#     def idk(self, x):
-#         pass
-#
-#
-# app = QApplication(sys.argv)
-# w = MainWindow()
-# w.show()
-# app.exec()
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(400, 130)
+
+        # # # # #
+
+        self.cbox1 = QComboBox()
+        self.cbox1.addItems(sorted(d.keys()))  # d.keys() 1!!! НЕТУ RUB
+        self.cbox1.setCurrentText('RUB')
+        self.cbox1.setFixedSize(190, 20)
+
+        self.cbox2 = QComboBox()
+        self.cbox2.addItems(sorted(d.keys()))  # d.keys() !!!! НЕТУ RUB
+        self.cbox2.setCurrentText('USD')
+        self.cbox2.setFixedSize(190, 20)
+
+        self.button = QPushButton('↑↓')
+
+        self.input1 = QLineEdit()
+        self.input1.setFixedSize(190, 20)
+        # self.input1.textChanged.connect()
+
+        self.input2 = QLineEdit()
+        self.input2.setFixedSize(190, 20)
+
+        # widget.setSuffix("c") 'asd' -> 'asd$'
+
+        self.box_vals = self.cbox1.currentText(), self.cbox2.currentText()
+
+        # # # # #
+
+        layout = QGridLayout()
+        layout.addWidget(self.cbox1, 0, 0)
+        # layout.addWidget(self.button, 1, 0)
+        layout.addWidget(self.cbox2, 2, 0)
+
+        layout.addWidget(self.input1, 0, 2)
+        layout.addWidget(self.input2, 2, 2)
+
+        #
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        # # # # #
+    def idk(self, x):
+        pass
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
